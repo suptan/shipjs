09eff678eaf061d5910f79c98f9af91cada427b6
Supachart Tansutiraphong <supachart.ta@easysunday.com>
2020-03-07 09:52:07 +0700

    initialize database

---
+++ src/migrations/20200307015818-create-map.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('maps', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      grid_vertical: {
+        type: Sequelize.STRING
+      },
+      grid_horizontal: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('maps');
+  }
+};
