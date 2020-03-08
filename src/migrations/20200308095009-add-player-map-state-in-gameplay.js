5dd48b65e75981beac5ef15f6105c1ac1a0415d1
suptan <s.tansutirapong@gmail.com>
2020-03-08 17:22:19 +0700

    add player map column

---
+++ src/migrations/20200308095009-add-player-map-state-in-gameplay.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('gameplay_players', 'player_map', {
+      type: Sequelize.ARRAY(Sequelize.INTEGER)
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('gameplay_players', 'player_map');
+  }
+};
