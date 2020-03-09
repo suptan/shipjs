ea8250a250c984813bea58d0a587bdc216ac20db
suptan <s.tansutirapong@gmail.com>
2020-03-09 21:31:43 +0700

    update README.md

---
+++ src/controllers/latest/ship.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import models from 'models';
+import asyncWrapper from 'middlewares/async-wrapper';
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /api/latest/ship:
+ *  get:
+ *    description: get all available ship
+ *    tags: [Ship]
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      200:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            data:
+ *              type: array
+ *              items:
+ *                $ref: '#definitions/baseShip'
+ */
+router.get('/ship', asyncWrapper(async (_, res) => {
+  const result = await models.ship.findAll();
+
+  res.status(!result ? 204 : 200).send({
+    data: result,
+  });
+}));
+
+export default router;
+
+/**
+ * @swagger
+ * tags:
+ *  - name: Ship
+ * definitions:
+ *  baseShip:
+ *    type: object
+ *    properties:
+ *      id:
+ *        type: integer
+ *      name:
+ *        type: string
+ *      size:
+ *        type: integer
+ *  ship:
+ *    allOf:
+ *      - $ref: '#definitions/baseShip'
+ */
